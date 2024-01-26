17145592555792da6b67c26196e04a26d10d3571
Tanjil Ahamed <tanjil113355@gmail.com>
2024-01-26 11:06:12 +0600

    about section responsive complete

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,10 +4,10 @@ import Btn from '../Shared/Btn';
 
 const About = () => {
     return (
-        <div id='about' className='my-40 text-white flex items-center justify-center'>
-            <div className='w-1/2'>
+        <div id='about' className='my-40 text-white flex gap-10 flex-col-reverse lg:flex-row items-center justify-center'>
+            <div className='lg:w-1/2 pl-5'>
                 <h2 className='text-[#55e6a5] text-xl font-bold'>About Me</h2>
-                <h1 className='text-3xl font-bold leading-10 mt-5'>
+                <h1 className='md:text-3xl text-2xl font-bold leading-10 mt-5'>
                     Transforming visions into <br />
                     exceptional portfolios
                 </h1>
@@ -21,7 +21,7 @@ const About = () => {
                 </div>
                 <Btn text={"Download Cv"}></Btn>
             </div>
-            <div className='w-1/2'>
+            <div className='lg:w-1/2'>
                 <div className='border-t-8 border-r-8 border-[#55e6a5] w-fit mx-auto'>
                     <Image
                         data-aos="fade-left"
