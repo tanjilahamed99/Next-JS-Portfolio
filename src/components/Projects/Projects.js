e127f71adb5cdad9623aa6b69d0f9f1d6e71f3ee
tanjilahamed99 <tanjil113355@gmail.com>
2025-01-28 15:09:47 +0600

    mew project added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import React from 'react';
 import luxeNext from '../../../public/LuxeNest.jpg'
 import nexTech from '../../../public/nexTech.jpg'
+import crypto from '../../../public/cryptocurrency-6601591_1920.jpg'
 import { FaGithub, FaLink } from 'react-icons/fa';
 
 const Projects = () => {
@@ -59,52 +60,29 @@ const Projects = () => {
                         </div>
                     </div>
                 </div>
-                {/* <div className='relative'>
-                    <Image src={luxeNext} alt='Projects logo'></Image>
+                <div className='relative'>
+                    <Image className='lg:h-[350px]' src={crypto} alt='Projects logo'></Image>
                     <div
-                        data-aos="fade-down"
+                        data-aos="fade-right"
                         data-aos-duration="3000"
                         className='bg-black bg-opacity-80 absolute w-full bottom-0'>
                         <div
                             className='py-5 pl-5'>
                             <div className='flex gap-2 items-center mb-3'>
-                                <h1 className='text-3xl font-light'>LuxeNest</h1>
-                                <h1 className='text-[#55e6a5]  text-sm font-bold'>|| Luxury Hotel</h1>
+                                <h1 className='text-3xl font-light'>Istimate Pro</h1>
+                                <h1 className='text-[#55e6a5]  text-sm font-bold'>|| Crypto Currency</h1>
                             </div>
                             <div className='flex gap-4 items-center'>
-                                <a href='https://github.com/tanjilahamed99/luxeNest-hotel-client-site'>
+                                <a href='https://github.com/tanjilahamed99/crypto-frontend'>
                                     <FaGithub className='bg-none text-3xl font-bold text-white hover:text-[#55e6a5]'></FaGithub>
                                 </a>
-                                <a href='https://luxenest-hotel.web.app'>
+                                <a href='https://crypto-frontend-woad.vercel.app'>
                                     <FaLink className='bg-none text-3xl font-bold text-white hover:text-[#55e6a5]'></FaLink>
                                 </a>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className='relative'>
-                    <Image src={luxeNext} alt='Projects logo'></Image>
-                    <div
-                        data-aos="fade-up"
-                        data-aos-duration="3000"
-                        className='bg-black bg-opacity-80 absolute w-full bottom-0'>
-                        <div
-                            className='py-5 pl-5'>
-                            <div className='flex gap-2 items-center mb-3'>
-                                <h1 className='text-3xl font-light'>LuxeNest</h1>
-                                <h1 className='text-[#55e6a5]  text-sm font-bold'>|| Luxury Hotel</h1>
-                            </div>
-                            <div className='flex gap-4 items-center'>
-                                <a href='https://github.com/tanjilahamed99/luxeNest-hotel-client-site'>
-                                    <FaGithub className='bg-none text-3xl font-bold text-white hover:text-[#55e6a5]'></FaGithub>
-                                </a>
-                                <a href='https://luxenest-hotel.web.app'>
-                                    <FaLink className='bg-none text-3xl font-bold text-white hover:text-[#55e6a5]'></FaLink>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div> */}
             </div>
         </div>
     );
